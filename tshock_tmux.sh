f308defb95105e3244a93a2b1ef6f4f203c836d2
edifus <edifus@users.noreply.github.com>
2014-09-11 11:28:06 -0600

    Update tshock_tmux.sh

--- tshock_tmux.sh
+++ tshock_tmux.sh
@@ -50,190 +50,190 @@ EXIT_TIMEOUT=30
 [ -f /lib/lsb/init-functions ] && . /lib/lsb/init-functions || exit 0
 
 checkTMUXSOCKET() {
-	${TMUXDAEMON} -S ${TMUXSOCKET} has-session > /dev/null 2>&1
-	return $?
+    ${TMUXDAEMON} -S ${TMUXSOCKET} has-session > /dev/null 2>&1
+    return $?
 }
 
 checkPID() {
-	PIDTOCHECK=$1
-	if [ -e ${PIDTOCHECK} ] && kill -0 $(cat ${PIDTOCHECK} 2>/dev/null) > /dev/null 2>&1; then
-		return 0
-	else
-		return 1
+    PIDTOCHECK=$1
+    if [ -e ${PIDTOCHECK} ] && kill -0 $(cat ${PIDTOCHECK} 2>/dev/null) > /dev/null 2>&1; then
+        return 0
+    else
+        return 1
 	fi
 }
 
 checkTMUXWINDOW() {
-	checkTMUXSOCKET
-	case "$?" in
-		0 )
-			WINDOWFOUND=$(${TMUXDAEMON} -S ${TMUXSOCKET} list-window | grep ${NAME})
-			if [ -z "${WINDOWFOUND}" ]; then
-				return 1
-			else
-				return 0
-			fi
-		;;
-		1 )
-			return 1
-		;;
-	esac
+    checkTMUXSOCKET
+    case "$?" in
+        0 )
+            WINDOWFOUND=$(${TMUXDAEMON} -S ${TMUXSOCKET} list-window | grep ${NAME})
+            if [ -z "${WINDOWFOUND}" ]; then
+                return 1
+            else
+                return 0
+            fi
+        ;;
+        1 )
+            return 1
+        ;;
+    esac
 }
 
 do_start() {
-	# Return
-	#   0 if daemon has been started
-	#   1 if daemon was already running
-	#   2 if daemon could not be started
-	start-stop-daemon --start --quiet \
-		--chdir ${TSHOCKDIR} \
-		--pidfile ${TMUXPID} \
-		--exec ${TMUXDAEMON} \
-		--test > /dev/null \
-		|| return 1
+    # Return
+    #   0 if daemon has been started
+    #   1 if daemon was already running
+    #   2 if daemon could not be started
+    start-stop-daemon --start --quiet \
+        --chdir ${TSHOCKDIR} \
+        --pidfile ${TMUXPID} \
+        --exec ${TMUXDAEMON} \
+        --test > /dev/null \
+        || return 1
 
-	checkTMUXSOCKET
-	case "$?" in
-		0 ) # tmux session found, start new window
-			start-stop-daemon --start --quiet \
-				--chdir ${TSHOCKDIR} \
-				--pidfile ${TMUXPID} \
-				--chuid ${USERID} \
-				--exec ${TMUXDAEMON} \
-				-- -2 -S ${TMUXSOCKET} new-window -d -n ${NAME} "${MONODAEMON} ${TERRARIA} ${TERRARIAARGS}" \
-				|| return 2
-		;;
-		1 ) # No tmux session found, start new session
-			start-stop-daemon --start --quiet \
-				--chdir ${TSHOCKDIR} \
-				--pidfile ${TMUXPID} \
-				--chuid ${USERID} \
-				--exec ${TMUXDAEMON} \
-				-- -2 -S ${TMUXSOCKET} new-session -d -n ${NAME} -s tShock "${MONODAEMON} ${TERRARIA} ${TERRARIAARGS}" \
-				|| return 2
-		;;
-	esac
+    checkTMUXSOCKET
+    case "$?" in
+        0 ) # tmux session found, start new window
+            start-stop-daemon --start --quiet \
+                --chdir ${TSHOCKDIR} \
+                --pidfile ${TMUXPID} \
+                --chuid ${USERID} \
+                --exec ${TMUXDAEMON} \
+                -- -2 -S ${TMUXSOCKET} new-window -d -n ${NAME} "${MONODAEMON} ${TERRARIA} ${TERRARIAARGS}" \
+                || return 2
+        ;;
+        1 ) # No tmux session found, start new session
+            start-stop-daemon --start --quiet \
+                --chdir ${TSHOCKDIR} \
+                --pidfile ${TMUXPID} \
+                --chuid ${USERID} \
+                --exec ${TMUXDAEMON} \
+                -- -2 -S ${TMUXSOCKET} new-session -d -n ${NAME} -s tShock "${MONODAEMON} ${TERRARIA} ${TERRARIAARGS}" \
+                || return 2
+        ;;
+    esac
 
-	# Create pidfile and set correct ownership
-	echo $(pgrep -u ${USERID} -f "${TMUXDAEMON} -2 -S ${TMUXSOCKET}") > ${TMUXPID}
-	chown ${USERID}:${USERID} ${TMUXPID}
-	chown ${USERID}:${USERID} ${TMUXSOCKET}
-	chmod 660 ${TMUXSOCKET} # owner and group can connect to tmux session
+    # Create pidfile and set correct ownership
+    echo $(pgrep -u ${USERID} -f "${TMUXDAEMON} -2 -S ${TMUXSOCKET}") > ${TMUXPID}
+    chown ${USERID}:${USERID} ${TMUXPID}
+    chown ${USERID}:${USERID} ${TMUXSOCKET}
+    chmod 660 ${TMUXSOCKET} # owner and group can connect to tmux session
 }
 
 do_stop() {
-	# Return
-	#   0 if daemon has been stopped
-	#   1 if daemon was already stopped
-	#   2 if daemon could not be stopped
-	RETVAL=2
-	if ! checkTMUXSOCKET; then
-		log_progress_msg "WARNING: tmux not running (tShock already stopped)"
-		echo "WARNING: tmux not running (tShock already stopped)"
-		RETVAL=1
-	elif  ! checkPID ${TSHOCKPID}; then
-		log_progress_msg "WARNING: tShock pidfile stale (tShock already stopped)"
-		echo "WARNING: tShock pidfile stale (tShock already stopped)"
-		RETVAL=1
-	else
-		echo "Please wait ${EXIT_WARNING} seconds to warn connected users..."
-		# Use 'send-keys' to push commands to tShock console
-		# Warn connected users server is shutting down
-		${TMUXDAEMON} -S ${TMUXSOCKET} send-keys -t tShock:${NAME} 'say Server shutting down in '${EXIT_WARNING}' seconds...' Enter
-		sleep ${EXIT_WARNING}
+    # Return
+    #   0 if daemon has been stopped
+    #   1 if daemon was already stopped
+    #   2 if daemon could not be stopped
+    RETVAL=2
+    if ! checkTMUXSOCKET; then
+        log_progress_msg "WARNING: tmux not running (tShock already stopped)"
+        echo "WARNING: tmux not running (tShock already stopped)"
+        RETVAL=1
+    elif  ! checkPID ${TSHOCKPID}; then
+        log_progress_msg "WARNING: tShock pidfile stale (tShock already stopped)"
+        echo "WARNING: tShock pidfile stale (tShock already stopped)"
+        RETVAL=1
+    else
+        echo "Please wait ${EXIT_WARNING} seconds to warn connected users..."
+        # Use 'send-keys' to push commands to tShock console
+        # Warn connected users server is shutting down
+        ${TMUXDAEMON} -S ${TMUXSOCKET} send-keys -t tShock:${NAME} 'say Server shutting down in '${EXIT_WARNING}' seconds...' Enter
+        sleep ${EXIT_WARNING}
 
-		# Send exit command to server
-		${TMUXDAEMON} -S ${TMUXSOCKET} send-keys -t tShock:${NAME} 'exit' Enter
+        # Send exit command to server
+        ${TMUXDAEMON} -S ${TMUXSOCKET} send-keys -t tShock:${NAME} 'exit' Enter
 		
-		# Wait for the process to end
-		n=${EXIT_TIMEOUT}
-		for (( i=0; i<n; i++ )); do
-			if ! checkTMUXWINDOW; then
-				rm -f ${TMUXPID}
-				if ! checkTMUXSOCKET; then rm -f ${TMUXSOCKET}; fi
-				RETVAL=0
-				break
-			fi
-			sleep 1
-		done
-	fi
-	return ${RETVAL}
+        # Wait for the process to end
+        n=${EXIT_TIMEOUT}
+        for (( i=0; i<n; i++ )); do
+            if ! checkTMUXWINDOW; then
+                rm -f ${TMUXPID}
+                if ! checkTMUXSOCKET; then rm -f ${TMUXSOCKET}; fi
+                RETVAL=0
+                break
+            fi
+            sleep 1
+        done
+    fi
+    return ${RETVAL}
 }
 
 do_status() {
-	if ! checkTMUXSOCKET; then
-		log_progress_msg "ERROR: (tmux not running)"
-		echo "ERROR: (tmux not running)"
-	elif ! checkTMUXWINDOW; then
-		log_progress_msg "ERROR: (tmux window '${NAME}' not found)"
-		echo "ERROR: (tmux window '${NAME}' not found)"
-	fi
-	status_of_proc -p ${TSHOCKPID} ${MONODAEMON} ${NAME} || return $?
+    if ! checkTMUXSOCKET; then
+        log_progress_msg "ERROR: (tmux not running)"
+        echo "ERROR: (tmux not running)"
+    elif ! checkTMUXWINDOW; then
+        log_progress_msg "ERROR: (tmux window '${NAME}' not found)"
+        echo "ERROR: (tmux window '${NAME}' not found)"
+    fi
+    status_of_proc -p ${TSHOCKPID} ${MONODAEMON} ${NAME} || return $?
 }
 
 do_connect() {
-	# Return
-	#   0 if connection successful
-	#   1 if tmux was not running/window not found
-	#   2 if connection was unsuccessful
-	if ! checkTMUXSOCKET; then
-		log_progress_msg "ERROR: Connect failed. (tmux not running)"
-		echo "ERROR: Connect failed. (tmux not running)" && return 1
-	elif ! checkTMUXWINDOW; then
-		log_progress_msg "ERROR: Connect failed. (tmux window '${NAME}' not found)"
-		echo "ERROR: Connect failed. (tmux window '${NAME}' not found)" && return 1	
-	else
-		# Select the correct tmux server window prior to connecting
-		${TMUXDAEMON} -S ${TMUXSOCKET} select-window -t tShock:${NAME}
-		# Connect to the tShock tmux session
-		${TMUXDAEMON} -S ${TMUXSOCKET} attach || return 2
-	fi
+    # Return
+    #   0 if connection successful
+    #   1 if tmux was not running/window not found
+    #   2 if connection was unsuccessful
+    if ! checkTMUXSOCKET; then
+        log_progress_msg "ERROR: Connect failed. (tmux not running)"
+        echo "ERROR: Connect failed. (tmux not running)" && return 1
+    elif ! checkTMUXWINDOW; then
+        log_progress_msg "ERROR: Connect failed. (tmux window '${NAME}' not found)"
+        echo "ERROR: Connect failed. (tmux window '${NAME}' not found)" && return 1	
+    else
+        # Select the correct tmux server window prior to connecting
+        ${TMUXDAEMON} -S ${TMUXSOCKET} select-window -t tShock:${NAME}
+        # Connect to the tShock tmux session
+        ${TMUXDAEMON} -S ${TMUXSOCKET} attach || return 2
+    fi
 }
 
 case "$1" in
-	start )
-		log_daemon_msg "Starting ${DESC}" "${NAME}"
-		do_start
-		case "$?" in
-			0 | 1 ) log_end_msg 0 ;; # Started successfully or already started
-			* ) log_end_msg 2 ;; # Failed to start
-		esac
-	;;
+    start )
+        log_daemon_msg "Starting ${DESC}" "${NAME}"
+        do_start
+        case "$?" in
+            0 | 1 ) log_end_msg 0 ;; # Started successfully or already started
+            * ) log_end_msg 2 ;; # Failed to start
+        esac
+    ;;
 
-	stop )
-		log_daemon_msg "Stopping ${DESC}" "${NAME}"
-		do_stop
-		case "$?" in
-			0 | 1 ) log_end_msg 0 ;; # Stopped successfully or already stopped
-			* ) log_end_msg 2 ;; # Failed to stop
-		esac
-	;;
+    stop )
+        log_daemon_msg "Stopping ${DESC}" "${NAME}"
+        do_stop
+        case "$?" in
+            0 | 1 ) log_end_msg 0 ;; # Stopped successfully or already stopped
+            * ) log_end_msg 2 ;; # Failed to stop
+        esac
+    ;;
 
-	restart )
-		log_daemon_msg "Restarting ${DESC}" "${NAME}"
-		do_stop
-		case "$?" in
-			0 | 1 ) # Stopped successfully or already stopped
-				do_start
-				case "$?" in
-					0 | 1 ) log_end_msg 0 ;; # Started successfully or already started
-					* ) log_end_msg 2 ;; # Failed to restart
-				esac
-			;;
-			* ) log_end_msg 2 ;; # Failed to stop
-		esac
-	;;
+    restart )
+        log_daemon_msg "Restarting ${DESC}" "${NAME}"
+        do_stop
+        case "$?" in
+            0 | 1 ) # Stopped successfully or already stopped
+                do_start
+                case "$?" in
+                    0 | 1 ) log_end_msg 0 ;; # Started successfully or already started
+                    * ) log_end_msg 2 ;; # Failed to restart
+                esac
+            ;;
+            * ) log_end_msg 2 ;; # Failed to stop
+        esac
+    ;;
 
-	status )
-		do_status && exit 0 || exit $?
-	;;
+    status )
+        do_status && exit 0 || exit $?
+    ;;
 
-	connect )
-		do_connect && exit 0 || exit $?
-	;;
+    connect )
+        do_connect && exit 0 || exit $?
+    ;;
 
-	* )
-		echo "Usage: $0 {start|stop|restart|status|connect}" 2>&1
-		exit 3
-	;;
+    * )
+        echo "Usage: $0 {start|stop|restart|status|connect}" 2>&1
+        exit 3
+    ;;
 esac
